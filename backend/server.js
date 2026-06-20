4cd85650c6a829b8fd4a6fa42dce5954faec6b5b
vidyanand29 <chorageprogramming2904@gmail.com>
2026-06-20 11:13:14 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors(
-{origin:process.env.CLIENT_URL}
-));
+cors({
+  origin: [
+    "http://localhost:5173",
+    "https://investo-business-website.vercel.app",
+  ],
+})
 
 app.use("/users", userRoutes);
 app.use("/business", businessIdeaRoutes);
